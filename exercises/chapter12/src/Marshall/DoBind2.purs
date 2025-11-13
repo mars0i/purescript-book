e30b7de8ce0b616e4bc1158e70515471aa2517a9
Marshall Abrams <mabrams001@bham.rr.com>
2025-11-13 12:14:06 -0600

    Recent experiments

---
+++ exercises/chapter12/src/Marshall/DoBind2.purs
@@ -0,0 +1,26 @@
+-- Exploring differences between do and >>=
+module Marshall.DoBind2 where
+
+import Prelude
+import Effect (Effect)
+import Data.Maybe (Maybe(..))
+import Graphics.Canvas (strokePath, rect, getContext2D, getCanvasElementById)
+import Partial.Unsafe (unsafePartial)
+
+-- See https://discord.com/channels/864614189094928394/1436973059423731772/1437043613790900386
+
+
+main :: Effect Unit
+main = void $ unsafePartial do
+  Just canvas <- getCanvasElementById "canvas"
+  ctx <- getContext2D canvas
+
+  strokePath ctx $ do
+     rect ctx {x: 350.0, y: 50.0,  width: 100.0, height: 100.0}
+     rect ctx {x: 350.0, y: 175.0, width: 100.0, height: 100.0}
+
+  strokePath ctx $ rect ctx {x: 250.0, y: 300.0, width: 100.0, height: 100.0} *>
+                   rect ctx {x: 250.0, y: 425.0, width: 100.0, height: 100.0}
+
+  strokePath ctx $ rect ctx {x: 150.0, y: 50.0,  width: 100.0, height: 100.0} >>=
+             \_ -> rect ctx {x: 150.0, y: 175.0, width: 100.0, height: 100.0}
